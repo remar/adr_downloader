b8a55accb2f14f16c2fde042e3510a061ffb401c
Andreas Remar <andreas.remar@gmail.com>
2017-03-10 11:23:29 +0100

    Return dict with post data

--- postdownloader.py
+++ postdownloader.py
@@ -73,13 +73,14 @@ def get_post(post):
 
     import re
 
-    date = re.findall(b"<h2 class='date-header'>.*?<span>(.*?)</span>.*?</h2>", data)[0]
+    date = re.findall(b"<h2 class='date-header'>.*?<span>(.*?)</span>.*?</h2>", data)[0].decode('utf-8')
 
-    print("DATE: \"" + date.decode('utf-8') + "\"")
+    print("DATE: \"" + date + "\"")
 
-    title = re.findall(b"<h3.*?>(.*?)</h3>", data, re.DOTALL)[0]
+    title = re.findall(b"<h3.*?>(.*?)</h3>", data, re.DOTALL)[0].decode('utf-8')
+    title = title.strip()
 
-    print("TITLE: \"" + title.decode('utf-8').strip() + "\"")
+    print("TITLE: \"" + title.strip() + "\"")
 
     start = re.search(b"<div class='post-body entry-content'", data)
     end = re.search(b"<div class='post-footer'>", data)
@@ -90,6 +91,9 @@ def get_post(post):
     parser.feed(content)
 
     #print("\n\n".join(parser.content))
+    name = (post.split("/")[-1]).split(".")[0]
+
+    return {"name":name, "title":title, "date":date, "content":content}
 
 if __name__ == "__main__":
-    get_post("file:///home/andreas/Projekt/adr_data/shouting.html")
+    print(get_post("file:///home/andreas/Projekt/adr_data/shouting.html")["title"])
