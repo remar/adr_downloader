e94fba4c852865f01f7d235a95a68ae0490365ad
Andreas Remar <andreas.remar@gmail.com>
2017-03-21 14:35:46 +0100

    Moved post parsing to own module

--- postdownloader.py
+++ postdownloader.py
@@ -1,4 +1,4 @@
-import urllib.request, re
+import urllib.request
 
 def get_post(url):
     print("Getting " + url)
@@ -6,39 +6,5 @@ def get_post(url):
     data = page.read()
     return data.decode('utf-8')
 
-def parse_post(post):
-    page = urllib.request.urlopen(post)
-    data = page.read()
-
-    date = re.findall(b"<h2 class='date-header'>.*?<span>(.*?)</span>.*?</h2>", data)[0].decode('utf-8')
-
-    title = re.findall(b"<h3.*?>(.*?)</h3>", data, re.DOTALL)[0].decode('utf-8')
-    title = title.strip()
-
-    start = re.search(b"<div class='post-body entry-content'", data)
-    end = re.search(b"<div class='post-footer'>", data)
-
-    content = data[start.start():end.start()].decode('utf-8')
-
-    name = (post.split("/")[-1]).split(".")[0]
-
-    return {"name":name, "title":title, "date":date, "content":content}
-
-def parse_date(date):
-    # Date comes in the form: "day_of_week, month day, year"
-    months = ["January", "February", "March", "April", "May", "June",
-              "July", "August", "September", "October", "November", "December"]
-    split = date.split(",")
-    year = split[2].strip()
-    month = str(months.index((split[1].strip().split(" ")[0])) + 1).zfill(2)
-    day = split[1].strip().split(" ")[1]
-    return year + "-" + month + "-" + day
-
 def get_path(post):
     return "/".join(post.split("/")[-3:])
-
-if __name__ == "__main__":
-    dates = ["Wednesday, November 16, 2016",
-             "Wednesday, September 05, 2012"]
-    for date in dates:
-        print(parse_date(date))
---
+++ postparser.py
@@ -0,0 +1,47 @@
+import re
+
+def parse_post(path):
+    name = (path.split("/")[-1]).split(".")[0]
+
+    f = open(path)
+    data = f.read()
+    f.close()
+
+    date = re.findall("<h2 class='date-header'>.*?<span>(.*?)</span>.*?</h2>", data)[0]
+
+    title = re.findall("<h3.*?>(.*?)</h3>", data, re.DOTALL)[0]
+    title = title.strip()
+
+    start = re.search("<div class='post-body entry-content'", data)
+    end = re.search("<div class='post-footer'>", data)
+
+    content = data[start.start():end.start()]
+
+    uid = parse_date(date) + "-" + name
+
+    return {"uid": uid, "title":title, "date":date, "content":content}
+
+def parse_date(date):
+    # Date comes in the form: "day_of_week, month day, year"
+    months = ["January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December"]
+    split = date.split(",")
+    year = split[2].strip()
+    month = str(months.index((split[1].strip().split(" ")[0])) + 1).zfill(2)
+    day = split[1].strip().split(" ")[1]
+    return year + "-" + month + "-" + day
+
+
+if __name__ == "__main__":
+    dates = ["Wednesday, November 16, 2016",
+             "Wednesday, September 05, 2012"]
+    for date in dates:
+        print(parse_date(date))
+
+    post = parse_post("data/raw/2006/05/after-prosthetic-society.html")
+
+    print("UID: \"" + post["uid"] + "\"")
+    print("Title: \"" + post["title"] + "\"")
+    print("Date: \"" + post["date"] + "\"")
+    
+    print("example path: data/txt/" + post["uid"] + ".txt")
